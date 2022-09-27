8bf3e1848eab36f04251ea8e7fa85e586d76a423
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-09-27 14:03:59 -0600

    padding and spacing

--- src/HomePage.js
+++ src/HomePage.js
@@ -8,10 +8,17 @@ export default function HomePage() {
         <h1>WELCOME TO AMANDAS ARCADE</h1>
       </div>
       <div className='body'>
-        <div className='game'>
-          <h2>Fruit Crush</h2>
-          <h4>Description</h4>
-          <a href="https://amandacrush.netlify.app/" target="/blank"><img src="/images/fruitCrush.png" className="icon" /></a>
+        <div className='games'>
+          <div className='fruitCrush'>
+            <h2>Fruit Crush</h2>
+            <h4>Match 3 or 4 game, like candy crush, but with fruit!</h4>
+            <a href="https://amandacrush.netlify.app/" target="/blank"><img src='/images/fruitCrush.png' className="picture" /></a>
+          </div>
+          <div className='minesweeper'>
+            <h2>Minesweeper</h2>
+            <h4>Description</h4>
+            <a href="https://amandacrush.netlify.app/" target="/blank"><img src='/images/fruitCrush.png' className="picture" /></a>
+          </div>
         </div>
       </div>
       <div className='footer'>
